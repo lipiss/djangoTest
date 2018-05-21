47f6e53862c3525de4fe180e2154851a5e37e8dc
Felipe <felipe.lipiss@gmail.com>
2018-05-21 14:58:27 +0200

    another commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+		'USER': 'tarsys',
+		'PASSWORD': 'tarsys',
+        'HOST': 'oselimw0206v.int.msdp.ericsson.se',
+        'PORT': '3306'
     }
 }
 
