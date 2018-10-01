2c24f32d9a0016ede81837eacf74a87a4f7ee832
Felipe <felipe.lipiss@gmail.com>
2018-10-01 15:51:56 +0200

    rendered file

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -31,52 +31,54 @@ def pullsectorlocation(request):
     # change default database.
     with connections['oselimw0201v.int.msdp.ericsson.se'].cursor() as cursor:
         #cursor = connections.cursor()
-        cursor.execute(''' SELECT
-    o3.name   AS 'BSC/RNC NAME',
-    o2.name AS 'SITE/NODEB NAME',
-    SUBSTRING_INDEX(o2.name, '-',-1) as enodeB,
-    cc.EUtranCellFDDId as EUtranCellFDDId,
-    o1.name  AS 'SECTOR/CELL NAME',
-    cc.GCID                 AS 'SECTOR/CELL GLOBAL ID',
-    '4G'                     AS 'TECHNOLOGY(2G/3G/4G)',
-    o1.vertical_loc/10e6   AS LATITUDE,
-    o1.horizontal_loc/10e6 AS LONGITUDE,
-    cc.azimuth              AS AZIMUTH,
-    cc.BeamWidth            AS BEAM_WIDTH,
-    cc.MTILT                AS MTILT,
-    cc.earfcndl,
-    3*cc.physicalLayerCellIdGroup+physicalLayerSubCellId as PCI
-    FROM
-    c_ecell cc
-    LEFT JOIN
-    timeslice_cm tc
-    ON
-    tc.timeslice_id = cc.timeslice_id
-    LEFT JOIN
-    objects o1
-    ON
-    o1.timeslice_id = tc.timeslice_id
-    AND
-    o1.int_id = cc.int_id
-    LEFT JOIN
-    objects o2
-    ON
-    o2.timeslice_id = tc.timeslice_id
-    AND
-    o1.parent_int_id = o2.int_id
-    LEFT JOIN
-    objects o3
-    ON
-    o3.timeslice_id = tc.timeslice_id
-    AND
-    o2.parent_int_id = o3.int_id
-    WHERE
-    cc.timeslice_id=tc.timeslice_id
-    AND
-    tc.period_start = (SELECT MAX(period_start) FROM timeslice_cm)
-    /*AND cc.EUtranCellFDDId <> o1.name*/
-    ORDER BY 
-    1,2,3 ''')
+        cursor.execute('''
+        SELECT
+        o3.name   AS 'BSC/RNC NAME',
+        o2.name AS 'SITE/NODEB NAME',
+        SUBSTRING_INDEX(o2.name, '-',-1) as enodeB,
+        cc.EUtranCellFDDId as EUtranCellFDDId,
+        o1.name  AS 'SECTOR/CELL NAME',
+        cc.GCID                 AS 'SECTOR/CELL GLOBAL ID',
+        '4G'                     AS 'TECHNOLOGY(2G/3G/4G)',
+        o1.vertical_loc/10e6   AS LATITUDE,
+        o1.horizontal_loc/10e6 AS LONGITUDE,
+        cc.azimuth              AS AZIMUTH,
+        cc.BeamWidth            AS BEAM_WIDTH,
+        cc.MTILT                AS MTILT,
+        cc.earfcndl,
+        3*cc.physicalLayerCellIdGroup+physicalLayerSubCellId as PCI
+        FROM
+        c_ecell cc
+        LEFT JOIN
+        timeslice_cm tc
+        ON
+        tc.timeslice_id = cc.timeslice_id
+        LEFT JOIN
+        objects o1
+        ON
+        o1.timeslice_id = tc.timeslice_id
+        AND
+        o1.int_id = cc.int_id
+        LEFT JOIN
+        objects o2
+        ON
+        o2.timeslice_id = tc.timeslice_id
+        AND
+        o1.parent_int_id = o2.int_id
+        LEFT JOIN
+        objects o3
+        ON
+        o3.timeslice_id = tc.timeslice_id
+        AND
+        o2.parent_int_id = o3.int_id
+        WHERE
+        cc.timeslice_id=tc.timeslice_id
+        AND
+        tc.period_start = (SELECT MAX(period_start) FROM timeslice_cm)
+        /*AND cc.EUtranCellFDDId <> o1.name*/
+        ORDER BY 
+        1,2,3
+        ''')
         row = cursor.fetchall()
     
     columns = [column[0] for column in cursor.description]
