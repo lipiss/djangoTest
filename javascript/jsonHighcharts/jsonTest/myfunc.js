1eaab0139d4faae745aada991f34214b38757702
lipiss <felipe.lipiss@gmail.com>
2018-12-27 09:28:46 +0100

    jsonHighcharts

---
+++ javascript/jsonHighcharts/jsonTest/myfunc.js
@@ -0,0 +1,11 @@
+var jsonSeries =[
+{name: 'LCS012A',data: [1.00,5.00,3.00,0.00]},
+{name: 'LCS012B',data: [0.00,0.00,0.00,0.00]},
+{name: 'LCS012C',data: [0.00,null,0.00,5]}
+];
+
+var jsonCategories = 
+['Feb-20-Mon 00h',
+'Feb-20-Mon 01h',
+'Feb-20-Mon 02h',
+'Feb-20-Mon 03h'];
